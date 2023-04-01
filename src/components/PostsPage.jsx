fb9f2a5ff51bc448a22e8dbadf6e783374514e2f
rhrkehfh <12204165@inha.edu>
2023-04-01 11:30:33 +0900

    ver03

--- src/components/PostsPage.jsx
+++ src/components/PostsPage.jsx
@@ -1,27 +1,35 @@
 import React, { useEffect, useState } from 'react'
-import { Row, Col, Table } from 'react-bootstrap';
+import { Row, Col, Table, Button } from 'react-bootstrap';
 
 const PostsPage = () => {
     const [list, setList] = useState([]);
 
     const [loading, setLoading] = useState(false);
 
+    const [page, setPage] = useState(1);
+
+    const [last, setLast] = useState(1);
+
     const getPosts = () => {
         setLoading(true);
         fetch('https://jsonplaceholder.typicode.com/todos')
-        .then(response => response.json())
-        .then(json => {
-            console.log(json);
-            setList(json);
-            setLoading(false);
-        })
+            .then(response => response.json())
+            .then(json => {
+                console.log(json);
+                setList(json);
+                let start = (page - 1) * 5 + 1;
+                let end = (page * 5);
+                setList(json.filter(post => post.id >= start && post.id <= end));
+                setLast(Math.ceil(json.length / 5));
+                setLoading(false);
+            })
     }
 
     useEffect(() => {
         getPosts();
-    }, []);
+    }, [page]);
 
-    if(loading) return <h1 className='text-center my-5'>로딩중</h1>
+    if (loading) return <h1 className='text-center my-5'>로딩중</h1>
     return (
         <Row className='justify-content-center'>
             <Col md={10}>
@@ -44,6 +52,15 @@ const PostsPage = () => {
                         )}
                     </tbody>
                 </Table>
+                <div className='text-center'>
+                    <Button
+                        disabled={page === 1 && true}
+                        onClick={() => setPage(page - 1)}>이전</Button>
+                    <span className='px-3'>{page}</span>
+                    <Button
+                        disabled={page === last && true}
+                        onClick={() => setPage(page + 1)}>다음</Button>
+                </div>
             </Col>
         </Row>
     )

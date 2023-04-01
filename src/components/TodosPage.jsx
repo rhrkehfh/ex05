bfa02e0b17ae3b40087954a66b7c1da69d517406
rhrkehfh <12204165@inha.edu>
2023-04-01 11:37:35 +0900

    ver04

--- src/components/TodosPage.jsx
+++ src/components/TodosPage.jsx
@@ -1,11 +1,71 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { Row, Col, Table, Button } from 'react-bootstrap';
 
 const TodosPage = () => {
-  return (
-    <div>
-        <h1>할일목록</h1>
-    </div>
-  )
+
+    const [list, setList] = useState([]);
+
+    const [loading, setLoading] = useState(false);
+
+    const [page, setPage] = useState(1);
+
+    const [last, setLast] = useState(1);
+
+    const getPosts = () => {
+        setLoading(true);
+        fetch('https://jsonplaceholder.typicode.com/todos')
+            .then(response => response.json())
+            .then(json => {
+                console.log(json);
+                setList(json);
+                let start = (page - 1) * 5 + 1;
+                let end = (page * 5);
+                setList(json.filter(post => post.id >= start && post.id <= end));
+                setLast(Math.ceil(json.length / 5));
+                setLoading(false);
+            })
+    }
+
+    useEffect(() => {
+        getPosts();
+    }, [page]);
+
+    if (loading) return <h1 className='text-center my-5'>로딩중</h1>
+    return (
+
+        <Row className='justify-content-center'>
+            <Col md={10}>
+                <h1 className='text-center my-5'>할일목록</h1>
+                <Table striped bordered hover>
+                    <thead>
+                        <tr className='text-center'>
+                            <td>ID.</td>
+                            <td>Title</td>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {list.map(posts =>
+                            <tr key={posts.id}>
+                                <td>{posts.id}</td>
+                                <td>
+                                    <div className='ellipsis'>{posts.title}</div>
+                                </td>
+                            </tr>
+                        )}
+                    </tbody>
+                </Table>
+                <div className='text-center'>
+                    <Button
+                        disabled={page === 1 && true}
+                        onClick={() => setPage(page - 1)}>이전</Button>
+                    <span className='px-3'>{page}</span>
+                    <Button
+                        disabled={page === last && true}
+                        onClick={() => setPage(page + 1)}>다음</Button>
+                </div>
+            </Col>
+        </Row>
+    )
 }
 
 export default TodosPage

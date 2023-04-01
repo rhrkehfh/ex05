20a5d584fbead00ed95e4e260c9599fb16588763
rhrkehfh <12204165@inha.edu>
2023-04-01 11:08:14 +0900

    ver02

--- src/components/PostsPage.jsx
+++ src/components/PostsPage.jsx
@@ -1,11 +1,52 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { Row, Col, Table } from 'react-bootstrap';
 
 const PostsPage = () => {
-  return (
-    <div>
-        <h1>게시글</h1>
-    </div>
-  )
+    const [list, setList] = useState([]);
+
+    const [loading, setLoading] = useState(false);
+
+    const getPosts = () => {
+        setLoading(true);
+        fetch('https://jsonplaceholder.typicode.com/todos')
+        .then(response => response.json())
+        .then(json => {
+            console.log(json);
+            setList(json);
+            setLoading(false);
+        })
+    }
+
+    useEffect(() => {
+        getPosts();
+    }, []);
+
+    if(loading) return <h1 className='text-center my-5'>로딩중</h1>
+    return (
+        <Row className='justify-content-center'>
+            <Col md={10}>
+                <h1 className='text-center my-5'>게시글</h1>
+                <Table striped bordered hover>
+                    <thead>
+                        <tr className='text-center'>
+                            <td>ID.</td>
+                            <td>Title</td>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {list.map(posts =>
+                            <tr key={posts.id}>
+                                <td>{posts.id}</td>
+                                <td>
+                                    <div className='ellipsis'>{posts.title}</div>
+                                </td>
+                            </tr>
+                        )}
+                    </tbody>
+                </Table>
+            </Col>
+        </Row>
+    )
 }
 
 export default PostsPage
